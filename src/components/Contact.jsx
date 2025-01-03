bf89989fa662dac96ccbde29d1d952db47869a81
Manohar Kale <manoharkale5368@gmail.com>
2025-01-03 17:44:13 +0530

    modified projects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <h2 className="text-3xl font-bold text-center text-gray-600 dark:text-white mb-12">CONTACT</h2>
         <div className='flex justify-center space-x-4'>
           <a href="mailto:manoharkale5368@gmail.com">
-            <button className='flex justify-center gap-3 items-center px-4 py-3 border border-1px-solid-blue text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition-colors'><Send size={18} />Email me </button>
+            <button className='flex justify-center gap-3 items-center px-4 py-3 border-0 text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 transition-colors'><Send size={18} />Email me </button>
           </a>
         </div>
       </div>
